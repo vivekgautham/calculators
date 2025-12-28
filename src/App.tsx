f1eaef204e6d51810190d37c208c1b62c4686377
Vivek <vivegau@gmail.com>
2025-12-28 11:46:40 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import { Typography } from '@mui/material';
+import React from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Typography } from "@mui/material";
 
 function App() {
   const [count, setCount] = React.useState<number>(0);
@@ -29,9 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more.....
       </p>
-      <Typography>
-        Salute
-      </Typography>
+      <Typography>Salute</Typography>
     </>
   );
 }
