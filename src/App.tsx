f9ab62c16eaf03995a4162bc86f123f7f8f49a2e
Vivek <vivegau@gmail.com>
2025-12-28 14:38:29 -0500

    Updates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-
 import "./App.css";
 import { Stack } from "@mui/material";
 import CalculatoryOutlet from "./components/CalculatorOutlet";
 
 function App() {
-
   return (
     <Stack direction={"row"} justifyContent={"space-between"}>
       <CalculatoryOutlet />
--- src/components/CalculatorOutlet.tsx
+++ src/components/CalculatorOutlet.tsx
@@ -14,37 +14,47 @@ import {
   Segment,
   Sidebar,
   MenuItemProps,
-} from 'semantic-ui-react'
+} from "semantic-ui-react";
 
 function CalculatoryOutlet() {
-    const [value, setValue] = React.useState<string | undefined>('unleveredbeta');
+  const [value, setValue] = React.useState<string | undefined>("unleveredbeta");
 
-    const handleChange = (_:  React.MouseEvent<HTMLAnchorElement, MouseEvent>, data: MenuItemProps) => {
-        setValue(data.name);
-    };
+  const handleChange = (
+    _: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    data: MenuItemProps,
+  ) => {
+    setValue(data.name);
+  };
 
-    return (
+  return (
     <Grid columns={2}>
-        <GridColumn>
-            <Sidebar
-                as={Menu}
-                animation='overlay'
-                icon='labeled'
-                inverted
-                vertical
-                visible={true}
-                width='wide'
-            >
-                <MenuItem name={'sharperatio'} active={value === "sharperatio"} onClick={handleChange}>
-                    Sharpe Ratio
-                </MenuItem>
-                <MenuItem name={'unleveredbeta'} active={value === "unleveredbeta"} onClick={handleChange}>
-                    Unlevered Beta
-                </MenuItem>
-            </Sidebar>
-
-        </GridColumn>
+      <GridColumn>
+        <Sidebar
+          as={Menu}
+          animation="overlay"
+          icon="labeled"
+          inverted
+          vertical
+          visible={true}
+          width="wide"
+        >
+          <MenuItem
+            name={"sharperatio"}
+            active={value === "sharperatio"}
+            onClick={handleChange}
+          >
+            Sharpe Ratio
+          </MenuItem>
+          <MenuItem
+            name={"unleveredbeta"}
+            active={value === "unleveredbeta"}
+            onClick={handleChange}
+          >
+            Unlevered Beta
+          </MenuItem>
+        </Sidebar>
+      </GridColumn>
     </Grid>
-    );
+  );
 }
 export default CalculatoryOutlet;
