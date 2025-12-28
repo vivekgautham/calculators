8e584ae433adf8d6eb1869ddc43de6e82a1a1719
Vivek <vivegau@gmail.com>
2025-12-28 11:36:46 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Typography } from '@mui/material';
 
 function App() {
   const [count, setCount] = React.useState<number>(0);
@@ -28,6 +29,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more.....
       </p>
+      <Typography>
+        Salute
+      </Typography>
     </>
   );
 }
