a8eb5bdbe3eb0309ec2367488893dfcad74f290f
Vivek <vivegau@gmail.com>
2025-12-28 12:56:13 -0500

    Updates.

---
+++ src/components/CalculatorOutlet.tsx
@@ -0,0 +1,29 @@
+import { Tab, Tabs, Box } from "@mui/material";
+import React from "react";
+
+function CalculatoryOutlet() {
+    const [value, setValue] = React.useState<number>(0);
+
+    const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+    <Box
+        sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224 }}
+        >
+        <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={value}
+            onChange={handleChange}
+            sx={{ borderRight: 1, borderColor: 'divider' }}
+        >
+            <Tab label="Item One" />
+            <Tab label="Item Two"  />
+        </Tabs>
+
+    </Box>
+    );
+}
+export default CalculatoryOutlet;
