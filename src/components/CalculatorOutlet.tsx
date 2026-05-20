3142d1b2f360da1bc52da349626a391b036523db
Vivek <vivegau@gmail.com>
2026-05-19 21:34:07 -0400

    Updates.

--- src/components/CalculatorOutlet.tsx
+++ src/components/CalculatorOutlet.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import {
   Divider,
   Header,
@@ -7,14 +7,51 @@ import {
   MenuItem,
   MenuItemProps,
   Search,
-  Sidebar
 } from "semantic-ui-react";
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, IconButton } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import { CALCULATORS_AND_SIMULATORS } from "../config";
 import { PanelProps } from "../types";
 
+const MIN_WIDTH = 200;
+const MAX_WIDTH = 600;
+const DEFAULT_WIDTH = 350;
+
 function CalculatoryOutlet() {
-  const [value, setValue] = React.useState<string | undefined>("rateofgrowth");
+  const [value, setValue] = useState<string | undefined>("rateofgrowth");
+  const [sidebarWidth, setSidebarWidth] = useState(DEFAULT_WIDTH);
+  const [isResizing, setIsResizing] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const startResizing = useCallback((mouseDownEvent: React.MouseEvent) => {
+    setIsResizing(true);
+  }, []);
+
+  const stopResizing = useCallback(() => {
+    setIsResizing(false);
+  }, []);
+
+  const resize = useCallback(
+    (mouseMoveEvent: MouseEvent) => {
+      if (isResizing) {
+        const newWidth = mouseMoveEvent.clientX;
+        if (newWidth >= MIN_WIDTH && newWidth <= MAX_WIDTH) {
+          setSidebarWidth(newWidth);
+        }
+      }
+    },
+    [isResizing]
+  );
+
+  useEffect(() => {
+    window.addEventListener("mousemove", resize);
+    window.addEventListener("mouseup", stopResizing);
+    return () => {
+      window.removeEventListener("mousemove", resize);
+      window.removeEventListener("mouseup", stopResizing);
+    };
+  }, [resize, stopResizing]);
 
   const handleChange = (
     _: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
@@ -23,41 +60,116 @@ function CalculatoryOutlet() {
     setValue(data.name);
   };
 
-  const currentPanel = CALCULATORS_AND_SIMULATORS.find((item) => item.value === value)?.panel
-  const name = CALCULATORS_AND_SIMULATORS.find((item) => item.value === value)?.name ?? 'Rate of Growth'
+  const currentPanel = CALCULATORS_AND_SIMULATORS.find((item) => item.value === value)?.panel;
+  const name = CALCULATORS_AND_SIMULATORS.find((item) => item.value === value)?.name ?? 'Rate of Growth';
 
   return (
     <Stack direction="row" sx={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-      <Box sx={{ width: '25vw', flexShrink: 0, height: '100%' }}>
-        <Sidebar
-          as={Menu}
-          animation="overlay"
-          icon="labeled"
-          inverted
-          vertical
-          visible={true}
-          style={{ width: '100%', position: 'relative' }}
+      {/* Sidebar Container */}
+      <Box
+        sx={{
+          width: isCollapsed ? 0 : sidebarWidth,
+          transition: isResizing ? 'none' : 'width 0.3s ease',
+          height: '100%',
+          position: 'relative',
+          backgroundColor: '#1b1c1d', // Match Semantic UI inverted menu
+          flexShrink: 0,
+          zIndex: 100,
+          borderRight: isResizing ? '2px solid #00b5ad' : 'none'
+        }}
+      >
+        <Box sx={{
+          width: sidebarWidth,
+          height: '100%',
+          overflowX: 'hidden',
+          display: isCollapsed ? 'none' : 'block'
+        }}>
+          <Menu
+            inverted
+            vertical
+            style={{
+              width: '100%',
+              height: '100%',
+              margin: 0,
+              borderRadius: 0,
+              border: 'none'
+            }}
+          >
+            <Divider />
+            <Header as='h3' color='teal' style={{ padding: '0 15px' }}>
+                Advanced Calculators & Simulators
+            </Header>
+            <Icon name='calculator' size='huge' color='teal' style={{ display: 'block', margin: '10px auto' }} />
+            <Divider />
+            <Box sx={{ p: '0 15px' }}>
+                <Search fluid />
+            </Box>
+            <Divider />
+            {CALCULATORS_AND_SIMULATORS.map((item) =>
+                <MenuItem
+                  key={item.name}
+                  name={item.value}
+                  active={value === item.value}
+                  onClick={handleChange}
+                  header={value === item.value}
+                >
+                  {item.name}
+                </MenuItem>
+            )}
+          </Menu>
+        </Box>
+
+        {/* Resize Handle */}
+        {!isCollapsed && (
+          <Box
+            onMouseDown={startResizing}
+            sx={{
+              width: '5px',
+              cursor: 'col-resize',
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              bottom: 0,
+              '&:hover': {
+                backgroundColor: 'rgba(0, 181, 173, 0.5)',
+              },
+            }}
+          />
+        )}
+
+        {/* Toggle Button */}
+        <IconButton
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          sx={{
+            position: 'absolute',
+            right: isCollapsed ? '-40px' : '5px',
+            top: '10px',
+            backgroundColor: '#1b1c1d',
+            color: '#00b5ad',
+            zIndex: 101,
+            border: '1px solid #00b5ad',
+            '&:hover': {
+              backgroundColor: '#00b5ad',
+              color: 'white',
+            },
+            width: '30px',
+            height: '30px',
+            transition: 'right 0.3s ease'
+          }}
+          size="small"
         >
-          <Divider />
-          <Header as='h3' color='teal'>Advanced Calculators & Simulators</Header>
-          <Icon name='calculator' size='huge' color='teal'> </Icon>
-          <Divider />
-          <Search fluid />
-          <Divider />
-          {CALCULATORS_AND_SIMULATORS.map((item) =>
-              <MenuItem
-                key={item.name}
-                name={item.value}
-                active={value === item.value}
-                onClick={handleChange}
-                header={value === item.value}
-              >
-                {item.name}
-              </MenuItem>
-          )}
-        </Sidebar>
+          {isCollapsed ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+        </IconButton>
       </Box>
-      <Box sx={{ width: '75vw', flexGrow: 1, height: '100%', overflow: 'hidden' }}>
+
+      {/* Main Content Area */}
+      <Box sx={{
+        flexGrow: 1,
+        height: '100%',
+        overflow: 'hidden',
+        width: isCollapsed ? '100vw' : `calc(100vw - ${sidebarWidth}px)`,
+        transition: isResizing ? 'none' : 'width 0.3s ease'
+      }}>
           {currentPanel && React.createElement(currentPanel, {name: name} as PanelProps)}
       </Box>
     </Stack>
